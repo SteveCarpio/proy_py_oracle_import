4458f8c552ccaf7345ad075f1df009b777d30534
Steve Carpio <carpios@tda-sgft.com>
2025-04-23 10:14:23 +0200

    inicio del proyecto

---
+++ src/BIACTIVOS_Master.py
@@ -0,0 +1,235 @@
+#------------------------------------------------------------------------------------------------
+# Programa: 
+#           Corte_Fichero_SubProcess_Master: 
+#               Orquestador de procesos, lanzará N.Hilos SubProcess definidos AdDoc.
+# SubProcess:
+#           Corte_Fichero_SubProcess_Slave:
+#               Ejecución de todos los hilos SubProcess.
+# Parámetros:
+#           1: Número de Hilo[n] SubProcess / N partes del File.txt
+#           2: Fichero de Entrada "FileN.txt" 
+#           3: Oracle Tabla Destino
+#           4: Oracle DNS sTv.BIACT_ora_dns
+#           5: Oracle UID sTv.BIACT_ora_uid
+#           6: Oracle PWD sTv.BIACT_ora_pwd
+# Librerías:
+#           Master:
+#               cfg: Parámetros comunes para el Master y Slave
+#               subprocess: Orquestador de SubProcess
+#               os: Interacción con el S.O. Windows
+#               sys: Iteración con los argumentos del Sistema
+#               datetime: Señales de tiempo
+#           Slave:
+#               cfg: Parámetros comunes para el Master y Slave
+#               pandas: Gestión analítica del fichero .txt en un DataFrame
+#               pyodbc: Inserción de datos del DataFrame en Oracle
+#               datetime: Señales de tiempo
+#               sys: Interactuar con los argumentos recibidos
+#                                                                          By --> SteveCarpio2024
+#------------------------------------------------------------------------------------------------
+
+# ------------- Librerías PERSONALIZADAS
+import cfg.PARAMETROS_COMUNES as sTv
+#-------------- Librerías MASTER
+import subprocess
+import os
+import sys
+from datetime import datetime as dt
+#-------------- Librerías SLAVE (dependencias para Compile)
+#import pandas as pd
+#import pyodbc
+#--------------
+
+# File de entrada de ANEXO3.txt
+file_entrada = f"C:\\MisCompilados\\file_in\\BIACTIVOS\\File.txt"
+
+# --------------------------------------------------------------------------
+
+# MODO: Desrrollo
+file_slave = f"C:\\Users\\scarpio\\Documents\\GitHub\\sTv_Publico\\CODE\\PYTHON\\SUBPROCESS\\BACTIVOS\\cfg\\BIACTIVOS_Slave.py"
+
+# MODO: Compile 
+#current_dir = os.path.dirname(sys.executable)
+#file_slave = os.path.join(current_dir, 'BIACTIVOS_Slave.exe')
+
+# --------------------------------------------------------------------------
+
+# Parametros Comunes
+tiempo_inicio = dt.now()
+tabla_txt = ["x"]
+procesos = []
+
+# Función que nos servirá para dividir el fichero de entrada ".txt" en varios partes/hilos
+def dividir_txt_x_partes():
+    ruta_archivo = file_entrada   
+    num_partes = numero_hilos 
+    
+    # Leer el contenido del archivo original  
+    with open(ruta_archivo, 'r') as archivo:  
+        lineas = archivo.readlines()
+
+    # Calcular el número de líneas por archivo
+    total_lineas = len(lineas)
+    lineas_por_parte = total_lineas // num_partes
+    sobrante = total_lineas % num_partes
+
+    # Directorio donde se guardarán los archivos divididos
+    directorio = os.path.dirname(ruta_archivo)
+
+    # Dividir el archivo en partes y guardarlas en nuevos archivos
+    inicio = 0
+    for i in range(1, num_partes + 1):
+        # Calcular el índice de fin para cada parte
+        fin = inicio + lineas_por_parte + (1 if i <= sobrante else 0)  # Agrega una línea extra a los primeros archivos si hay sobrante
+        nombre_parte = f"File{i}.txt"
+        ruta_parte = os.path.join(directorio, nombre_parte)
+        
+        # Escribir cada parte en un nuevo archivo
+        with open(ruta_parte, 'w') as archivo_parte:
+            archivo_parte.writelines(lineas[inicio:fin])
+        
+        # Actualizar el índice de inicio para la siguiente parte
+        inicio = fin
+
+        # Crea lista con los nombres de salida
+        tabla_txt.append(f"File{i}")
+    # Imprimir un mesaje para la log
+    print(f"  El archivo de entrada tiene {len(lineas):,} registros dividido en {numero_hilos} partes.".replace(",","."))
+
+# Función que nos lanzará los SUBPROCESS de la lista "procesos[...]" 
+def ejecucion_multi_hilo():
+    print(f"  Invocado {numero_hilos} Hilos SubProcess.\n")
+    # Ejecutar cada proceso sin esperar
+    for i in range(1, numero_hilos + 1):
+        # Iniciar el proceso y pasar el argumento
+        # Desarrollo
+        proceso = subprocess.Popen(["python", file_slave, str(i), tabla_txt[i], str(tabla_ora), oracle_dns, oracle_uid, oracle_pwd, str(var_periodo)])  # Desarrollo
+        # Compile
+        #proceso = subprocess.Popen([file_slave, str(i), tabla_txt[i], str(tabla_ora), oracle_dns, oracle_uid, oracle_pwd])            # Compile
+        procesos.append(proceso)  
+    # Esperar a que todos los procesos terminen
+    for proceso in procesos:
+        proceso.wait()
+
+# Función necesario para validar si existe el file.txt de entrada
+def validar_existe_txt():
+    valida_si_existe = os.path.isfile(file_entrada)
+    if valida_si_existe == False:
+        print(f"¡¡¡ ERROR: No existe el fichero {file_entrada} !!!")
+    return valida_si_existe  # True or False
+
+### -------------------------------- Inicio del programa ----------------------------
+
+oracle_dns   = sTv.BIACT_ora_dns
+oracle_uid   = sTv.BIACT_ora_uid
+oracle_pwd   = sTv.BIACT_ora_pwd
+tabla_ora    = sTv.BIACT_tab_ora
+numero_hilos = sTv.BIACT_num_hil
+
+xyear = dt.now().year
+xmonth = dt.now().month
+var_periodo = int(f'{xyear}{xmonth:02d}')
+
+# ---------------------------------- MENU 
+os.system('cls')
+print(f" CORTE DE FICHERO DE BIACTIVOS\n -----------------------------\n")
+print(f"   Parámetros por defecto:\n")
+print(f"     Número de Hilos: {numero_hilos}")
+print(f"     Tabla Oracle:    {tabla_ora}")
+print(f"     Oracle DNS:      {oracle_dns}")
+print(f"     Oracle UID:      {oracle_uid}")
+print(f"     Oracle PWD:      {oracle_pwd}")
+print(f"     Periodo AAAAMM:  {var_periodo}\n")
+
+sw=0
+tmp1=""
+tmp2=""
+tmp3=""
+tmp4=""
+tmp5=""
+tmp6=""
+continuar = "n"
+continuar = input("¿ Quiere cambiar, Número de hilos (s/n) ?:    ")
+if continuar == "s" or continuar == "S":
+    numero_hilos = int(input(f"  ¡ Indique número de hilos! [{numero_hilos}]: "))
+    if numero_hilos < 2 or numero_hilos > 18:
+        print("    ¡Atención! Valor fuera del rango, usar valores del (2 ..al.. 18) hilos")
+        print(f"      Se usará el valor predeterminado: {sTv.BIACT_num_hil}")
+        numero_hilos = sTv.BIACT_num_hil
+    sw=1
+    tmp1="*"
+
+continuar = "n"
+continuar = input("¿ Quiere cambiar, Tabla Oracle (s/n) ?:       ")
+if continuar == "s" or continuar == "S":
+    tabla_ora = input(f"  ¡ Indique el nombre de la Tabla Oracle! [{tabla_ora}]: ")
+    sw=1
+    tmp2="*"
+
+continuar = "n"
+continuar = input("¿ Quiere cambiar, DNS Oracle (s/n) ?:         ")
+if continuar == "s" or continuar == "S":
+    oracle_dns = input(f"  ¡ Indique el DNS Oracle! [{oracle_dns}]: ")
+    sw=1
+    tmp3="*"
+
+continuar = "n"
+continuar = input("¿ Quiere cambiar, UID Oracle (s/n) ?:         ")
+if continuar == "s" or continuar == "S":
+    oracle_uid = input(f"  ¡ Indique el UID Oracle! [{oracle_uid}]: ")
+    sw=1
+    tmp4="*"
+
+continuar = "n"
+continuar = input("¿ Quiere cambiar, PWD Oracle (s/n) ?:         ")
+if continuar == "s" or continuar == "S":
+    oracle_pwd = input(f"  ¡ Indique el PWD Oracle! [{oracle_pwd}]: ")
+    sw=1
+    tmp5="*"
+
+continuar = "n"
+continuar = input("¿ Quiere cambiar, Periodo AAAAMM (s/n) ?:     ")
+if continuar == "s" or continuar == "S":
+    var_periodo = int(input(f"  ¡ Indique el Periodo! [{var_periodo}]: "))
+    sw=1
+    tmp6="*"
+
+if sw == 1:
+    os.system('cls')
+    print(f" CORTE DE FICHERO DE BIACTIVOS\n -----------------------------\n")
+    print(f"   Parámetros modificados (*):\n")
+    print(f"     Número de Hilos: {tmp1}{numero_hilos}{tmp1}")
+    print(f"     Tabla Oracle:    {tmp2}{tabla_ora}{tmp2}")
+    print(f"     Oracle DNS:      {tmp3}{oracle_dns}{tmp3}")
+    print(f"     Oracle UID:      {tmp4}{oracle_uid}{tmp4}")
+    print(f"     Oracle PWD:      {tmp5}{oracle_pwd}{tmp5}")
+    print(f"     Periodo AAAAMM:  {tmp6}{var_periodo}{tmp6}\n")
+
+
+continuar = "n"
+continuar = input("\n¿ EJECUTAMOS EL PROCESO (s/n) ?: ")
+if continuar == "s" or continuar == "S":
+    print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - ")
+
+    # ---------------------------------- EJECUCION DEL PROCESO
+    # Imprimir incio del SubProcess multi-hilo
+    print(f"\nINICIO - SUBPROCESS-MULTI HILO.... {tiempo_inicio} \n")
+
+    # Llamar a las funciones
+    existe_file = validar_existe_txt()
+    if existe_file:
+        dividir_txt_x_partes()
+        ejecucion_multi_hilo()
+
+    # Calcular el Tiempo Total transcurrido del proceso
+    diferencia_total = dt.now() - tiempo_inicio
+    minutos_totales = diferencia_total.total_seconds() // 60  # Minutos totales : // División entera
+    segundos_restantes = round(diferencia_total.total_seconds() % 60, 2) # Segundos restantes y redondear a 2 decimales
+
+    # Imprimir fin del SubProcess multi-hilo
+    print(f"\nFIN - SUBPROCESS-MULTI HILO.... {dt.now()} ---> {int(minutos_totales)}min {segundos_restantes:.0f}seg \n")
+    continuar = input("\n\n --- FIN --- ")
+else:
+    print(" --- EXIT --- ")
+
+### --------------------------------- Fin del programa ------------------------------
---
+++ src/biactivos/BIACTIVOS_Slave.py
@@ -0,0 +1,147 @@
+#------------------------------------------------------------------------------------------------
+# SubProcess:
+#           Corte_Fichero_SubProcess_Slave:
+#               Ejecución de todos los hilos SubProcess.
+# Parámetros:
+#           1: Número de Hilo[n] SubProcess / N partes del File.txt
+#           2: Fichero de Entrada "FileN.txt" 
+#           3: Oracle Tabla Destino
+#           4: Oracle DNS sTv.BIACT_ora_dns
+#           5: Oracle UID sTv.BIACT_ora_uid
+#           6: Oracle PWD sTv.BIACT_ora_pwd
+# Librerías:
+#           Master:
+#               cfg: Parámetros comunes para el Master y Slave
+#               subprocess: Orquestador de SubProcess
+#               os: Interacción con el S.O. Windows
+#               datetime: Señales de tiempo
+#           Slave:
+#               pandas: Gestión analítica del fichero .txt en un DataFrame
+#               pyodbc: Inserción de datos del DataFrame en Oracle
+#               datetime: Señales de tiempo
+#               sys: Interactuar con los argumentos recibidos
+#                                                                          By --> SteveCarpio2024
+#------------------------------------------------------------------------------------------------
+
+# Carga de Librerías
+import pandas as pd
+import pyodbc
+import sys
+from datetime import datetime as dt
+
+#print(f"Parametro0:  {sys.argv[0]}")
+#print(f"Parametro1:  {sys.argv[1]}")
+#print(f"Parametro2:  {sys.argv[2]}")
+#print(f"Parametro3:  {sys.argv[3]}")
+#print(f"Parametro4:  {sys.argv[4]}")
+#print(f"Parametro5:  {sys.argv[5]}")
+#print(f"Parametro6:  {sys.argv[6]}")
+#print(f"Parametro6:  {sys.argv[7]}")
+
+#if __name__ == "__main__":
+if len(sys.argv) == 8:  # 8
+    PAR0 = sys.argv[0]         # Nombre del fichero de ejecución
+    PAR1 = sys.argv[1]         # Número de Hilo
+    PAR2 = sys.argv[2]         # Tabla de Entrada "FileN" sin extención '.txt'
+    PAR3 = sys.argv[3]         # Oracle Tabla
+    PAR4 = sys.argv[4]         # Oracle DNS sTv.BIACT_ora_dns
+    PAR5 = sys.argv[5]         # Oracle UID sTv.BIACT_ora_uid
+    PAR6 = sys.argv[6]         # Oracle PWD sTv.BIACT_ora_pwd
+    PAR7 = sys.argv[7]         # PERIODO AAAAMM
+
+    if len(PAR1) == 1:
+        PAR11=f"0{PAR1}" 
+    else:
+        PAR11=f"{PAR1}"
+
+    ### ----------------------------------- Funciones -----------------------------------
+
+    # 1 - def Oracle_Establece_Conexion(par_dsn , par_uid , par_pwd) 
+    def Oracle_Establece_Conexion(par_dsn, par_uid,par_pwd):
+        try:
+            # Cadena de conexión a la base de datos Oracle
+            connection_string = f'DSN={par_dsn};UID={par_uid};PWD={par_pwd};'
+            # Establecer la conexión y un cursor a la base de datos Oracle
+            conexion = pyodbc.connect(connection_string)
+            cursor = conexion.cursor()
+        # cursor.fast_executemany = True  <--- es para SQL Server aquí no me vale
+            print(f"    Hilo[{PAR11}] - {dt.now().time()} - Conexión establecida.... ")
+            return conexion, cursor
+        except pyodbc.Error as e:
+            print(f'    Hilo[{PAR11}] - {dt.now().time()} - Error al conectar con Oracle: {e}')
+            return None, None 
+
+    # 2 - def Oracle_Cerrar_Conexion(conexion , cursor)
+    def Oracle_Cerrar_Conexion(conexion, cursor):
+        try:
+            if cursor:
+                cursor.close()
+            if conexion:
+                conexion.close()
+            print(f"    Hilo[{PAR11}] - {dt.now().time()} - Conexión cerrada........ ")
+        except pyodbc.Error as e:
+            print(f'    Hilo[{PAR11}] - Error al cerrar la conexión: {e}')
+
+    ### -------------------------------- Inicio del programa ----------------------------
+
+    # Oracle, Parámetros de conexión:
+    oracle_dns=PAR4
+    oracle_uid=PAR5
+    oracle_pwd=PAR6
+
+    # Oracle, Establecer Conexión Oracle:
+    conexion, cursor=Oracle_Establece_Conexion(oracle_dns, oracle_uid, oracle_pwd)
+
+    if (conexion != None) or (cursor != None):
+        # Parámetros otros, file 'txt' y variables de apoyo:
+        var_periodo = PAR7    # var_periodo = int(f'{dt.now().year}{dt.now().month}')
+        var_fecha_proceso = dt.now().date()
+        files_csv = PAR2
+        file_path = f"C:\\MisCompilados\\file_in\\BIACTIVOS\\{files_csv}.txt"
+
+        # Importación del fichero "FileN.txt" en un DataFrame:
+        df = pd.read_csv(file_path, sep='\t', header=None,
+            names=['NUMERO_DE_OPERACION', 'NUMERO_DE_CUOTA', 'FECHA_DE_VENCIMIENTO', 'CAPITAL', 'INTERES', 'IVA',
+                'TOTAL_CUOTA','TOTAL_RECAUDADO', 'FECHA_DE_PAGO', 'FECHA_DESCUENTO_DE_NOMINA', 'ESTADO_DEL_PAGO'],
+            dtype={'NUMERO_DE_OPERACION': str, 'NUMERO_DE_CUOTA': int, 'CAPITAL': float, 'INTERES': float, 'IVA': float, 
+                'TOTAL_CUOTA':float, 'TOTAL_RECAUDADO':float,'ESTADO_DEL_PAGO':str ,'ESTADO_DEL_PAGO':str},
+            parse_dates=['FECHA_DE_VENCIMIENTO','FECHA_DE_PAGO','FECHA_DESCUENTO_DE_NOMINA'],
+            dayfirst=True
+        )
+
+        # Imprime log del estado del Hilo
+        print(f'    Hilo[{PAR11}] - {dt.now().time()} - Insert Into Oracle {PAR3} {len(df):,} records From {PAR2}.txt'.replace(",","."))
+
+        ### -------------------------------------- Oracle -----------------------------------
+        # Parámetros necesarios para Exportación del DataFrame al Oracle de TdA:
+        table_name = PAR3
+
+        # Oracle, Recorro el DataFrame registro por registro
+        for index, row in df.iterrows():
+            # Tratamiento de valores NaT a None
+            tmpFECHA1=row['FECHA_DE_PAGO']
+            tmpFECHA2=row['FECHA_DESCUENTO_DE_NOMINA']
+            if pd.isna(tmpFECHA1):
+                tmpFECHA1=None
+            if pd.isna(tmpFECHA2):
+                tmpFECHA2=None    
+            # Ejecución del INSERT SQL
+            cursor.execute(
+                f"""INSERT INTO {table_name} (NUMERO_DE_OPERACION, NUMERO_DE_CUOTA, FECHA_DE_VENCIMIENTO, CAPITAL, INTERES, IVA,
+                TOTAL_CUOTA, TOTAL_RECAUDADO, FECHA_DE_PAGO, FECHA_DESCUENTO_DE_NOMINA, ESTADO_DEL_PAGO, PERIODO, FECHA_PROCESO) 
+                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                row['NUMERO_DE_OPERACION'], row['NUMERO_DE_CUOTA'], row['FECHA_DE_VENCIMIENTO'], row['CAPITAL'], row['INTERES'], 
+                row['IVA'], row['TOTAL_CUOTA'], row['TOTAL_RECAUDADO'], tmpFECHA1, tmpFECHA2, 
+                row['ESTADO_DEL_PAGO'], var_periodo, var_fecha_proceso
+            )
+
+        # Oracle, Confirma los cambios
+        conexion.commit()
+        print(f'    Hilo[{PAR11}] - {dt.now().time()} - Commit OK')
+
+        # Oracle, Cierre de conexiones y liberación de memoria:
+        Oracle_Cerrar_Conexion(conexion, cursor)
+        ### --------------------------------- Fin del programa ------------------------------
+else:
+    print("  ¡¡¡ AVISO: Este código es una rutina de SUBPROCESS !!!")
+    print("se debe ejecutar desde: Corte_fichero_SubProcess_Master.py")
